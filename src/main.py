8f8d5dbf5fa30b43a79315d5e0bc1f35ab1fc2cd
zhangmu <zhangmu@MacBook-Pro.local>
2025-09-19 13:46:24 +0800

    Initial python project commit

---
+++ src/main.py
@@ -0,0 +1,20 @@
+# Import the FastAPI class from the fastapi module.
+from fastapi import FastAPI
+
+# Declare an instance of the FastAPI class.
+app = FastAPI()
+
+@app.get("/")
+def read_index():
+    """
+    Return a Python Dictionary that supports JSON serialization.
+    """
+    return {"Hello": "World"}
+
+@app.get("/api/v1/hello-world/")
+def read_hello_world():
+    """
+    Return an API-like response.
+    """
+    return {"what": "road", "where": "kubernetes", "version": "v1"}
+
